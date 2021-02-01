d21b6aa25262b01b534ae4665bacbc7c8a6cf5ea
Tom Abraham <tomabraham6279@gmail.com>
2021-02-01 03:09:10 -0800

    Created basic tableview and connected provided REST API to IOS app. Next step: Populate tableview with information from REST API.

---
+++ AggieFeed/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  AggieFeed
+//
+//  Created by Tom Abraham on 2/1/21.
+//
+
+import Foundation
+
+struct Post: Codable{
+    var title: String!
+}
--- AggieFeed/ViewController.swift
+++ AggieFeed/ViewController.swift
@@ -8,12 +8,100 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    let names = [
+        "John Smith",
+        "Dan Smith",
+        "Jason Smith",
+        "Mary Smith"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        fetchPostData { (posts) in
+            for post in posts {
+                print(post.title!)
+            }
+        }
+        
     }
+    
+    func fetchPostData(completionHandler: @escaping ([Post]) -> Void) {
+        let url = URL(string: "https://aggiefeed.ucdavis.edu/api/v1/activity/public?s=0?l=25")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else { return }
+            
+            do {
+                
+                let postsData = try JSONDecoder().decode([Post].self, from: data)
+                
+                completionHandler(postsData)
+            }
+            catch {
+                let error = error
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
+    
+    
+    
+  
 
 
+}
+
+
+// MARK: - WelcomeElement
+//struct WelcomeElement: Codable {
+//    var title: String!
+//    public let actor: Actor
+//    public let object: Object
+//    public let published: String
+//
+//}
+
+//// MARK: - Actor
+//struct Actor: Codable {
+//    public let displayName: String
+//}
+//
+//// MARK: - Object
+//struct Object: Codable {
+//    public let objectType: ObjectObjectType
+//
+//}
+//
+//enum ObjectObjectType: String, Codable {
+//    case event = "event"
+//    case notification = "notification"
+//}
+
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me!")
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
 }
 
