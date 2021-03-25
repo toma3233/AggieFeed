cf146b0de5df9f97fdbca26ef58916d8d8791fa1
Tom Abraham <tomabraham6279@gmail.com>
2021-03-25 13:04:52 -0700

    Able to display cached News items on tableview now, full caching function essentially done

--- AggieFeed/ViewController.swift
+++ AggieFeed/ViewController.swift
@@ -31,32 +31,8 @@ class ViewController: UIViewController {
         
         // Calls function to scan each activity and append to array
         fetchPostData { (posts) in }
-        
-        // Get items from CoreData
-        //fetchNews()
+      
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-//        let results = database.fetch(type: News.self)
-//        print(results.map { $0.news_title })
-    }
-    
-    
-    
-    func fetchNews() {
-        // Fetch the data from CoreData
-        do {
-            self.items = try context.fetch(News.fetchRequest())
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
-        catch {
-            
-        }
-    }
-    
     
     // Use completion handler to avoid loading issues
     func fetchPostData(completionHandler: @escaping ([Post]) -> Void) {
@@ -69,11 +45,8 @@ class ViewController: UIViewController {
             do {
                 // Decodes each byte of data according to codable structs created in Post.swift
                 let postsData = try JSONDecoder().decode([Post].self, from: data)
-                
-                
-                
-               
-                
+  
+                // Deletes everything in the CoreData DB when reading from API
                 if self.database.deleteAll() {
                     let obj = self.database.fetch(type: News.self)
                     print("AMT OF ITEMS AFTER DELETION")
@@ -84,65 +57,41 @@ class ViewController: UIViewController {
                     print(obj.map { $0.news_title })
                 }
                 
-            
-                
-                
-                
-                // How do I create a newNewsItem object for every Post object?
-                
                 // appends each activity to activtyArray
                 self.activityArray.append(contentsOf: postsData)
                 for index in 0...(self.activityArray.count - 1) {
+                    
+                    // Print out contents of db for debugging purposes
                     let pbj = self.database.fetch(type: News.self)
                     print("Before COUNT")
                     print(pbj.count)
+                    
                     let newNewsItem = News(context: self.context)
                     newNewsItem.news_displayName = self.activityArray[index].actor.displayName
                     newNewsItem.news_title = self.activityArray[index].title
                     newNewsItem.news_objectType = self.activityArray[index].object.objectType.map { $0.rawValue }
                     newNewsItem.news_published = self.activityArray[index].published
-                    
-                    //self.database.add(type: News.self)
-                    
-//                    guard let news = self.database.add(type: News.self) else { return }
-//                    news.news_displayName = self.activityArray[index].actor.displayName
-//                    news.news_title = self.activityArray[index].title
-//                    news.news_objectType = self.activityArray[index].object.objectType.map { $0.rawValue }
-//                    news.news_published = self.activityArray[index].published
-                    
-                    //print(news.news_title)
+                     
+                    // Print out contents of db for debugging purposes
                     for i in pbj {
-                     print(i.news_title)
+                        print(i.news_title!)
                     }
                     print("After COUNT")
                     print(pbj.count)
                     print(" ")
                     print(" ")
                     
-                    
-                    
-                    
-                    //print(newNewsItem.news_title!)
                     self.items.append(newNewsItem)
                     self.database.save()
-                    //print(self.items.count)
                 }
+                
+                // Print out everything in database once API has been read
                 let pbj = self.database.fetch(type: News.self)
                 print("NEW COUNT")
                 print(pbj.count)
                 for i in pbj {
-                    print(i.news_title)
+                    print(i.news_title!)
                 }
-//
-                
-                
-                
-                
-                
-                
-                
-                //newNewsItem.news_objectType = self.activityArray[self.index].object.objectType
-                
                 
                 do {
                     try self.context.save()
@@ -151,20 +100,25 @@ class ViewController: UIViewController {
                     
                 }
                 
-                //self.fetchNews()
-                
-                
                 completionHandler(postsData)
             }
             catch {
-                // exception handling
                 
+                // Account for no internet connect and failed API connection
                 print("API Connection Failed")
                 let pbj = self.database.fetch(type: News.self)
-                print("NEW COUNT")
+                print("NUM ITEMS IN DB")
                 print(pbj.count)
                 for i in pbj {
-                    print(i.news_title)
+                    print(i.news_title!)
+                }
+                
+                // populate items array with News objects from CoreData DB
+                let req = NSFetchRequest<News>(entityName: "News")
+                do {
+                    self.items = try self.context.fetch(req)
+                } catch {
+                    print ("DIDNT WORK")
                 }
             }
             DispatchQueue.main.async {
@@ -202,12 +156,7 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        // Prints out title and actor.displayName for each table view cell
-//        let activity = activityArray[indexPath.row]
         let news_item = self.items[indexPath.row]
-//        cell.textLabel?.text = activity.title
-//        cell.detailTextLabel?.text = activity.actor.displayName
         cell.textLabel?.text = news_item.news_title
         cell.detailTextLabel?.text = news_item.news_displayName
         return cell
